e65920766b699741d92cc003d5e72c5ba1872116
chelaxian <chelaxian@gmail.com>
2026-06-30 22:26:51 +0300

    Publish Instagram grid feed 0.2.0

--- source/Instagram3x3GridFeed/build.sh
+++ source/Instagram3x3GridFeed/build.sh
@@ -10,4 +10,15 @@ export THEOS_PACKAGE_SCHEME=roothide
 make clean
 make package FINALPACKAGE=1
 
+pkg="$(ls -t packages/com.ratush.iggridfeed_*_iphoneos-arm64e.deb | head -1)"
+work="$(mktemp -d)"
+dpkg-deb -R "$pkg" "$work"
+if [ -d layout/DEBIAN ]; then
+  cp -f layout/DEBIAN/* "$work/DEBIAN/"
+  chmod 0755 "$work/DEBIAN/"*
+fi
+(cd "$work" && find . -path ./DEBIAN -prune -o -type f -print0 | sort -z | while IFS= read -r -d '' f; do md5sum "$f" | sed 's#  ./#  #'; done > DEBIAN/md5sums)
+dpkg-deb -Zgzip --root-owner-group -b "$work" "$pkg"
+rm -rf "$work"
+
 ls -la packages/*.deb
